7af3288b0ede5dd56a21616eb213c4869ec41114
Abhinav <abhinav.r1296@gmail.com>
2026-07-10 22:10:41 +0530

    added navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+// import PlaceOrder from './pages/PlaceOrder'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
-      Hello Abhinav
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:[9vw]'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/collection' element={<Collection />} />
+        {/* <Route path='/about' element = {<About/>} />
+        <Route path='/contact' element = {<Contact/>} />
+        <Route path='/product/:productId' element = {<Product/>} />
+        <Route path='/Cart' element = {<Cart/>} />
+        <Route path='/Login' element = {<Login/>} />
+        <Route path='/place-order' element = {<PlaceOrder/>} />
+        <Route path='/orders' element = {<Orders/>} /> */}
+      </Routes>
     </div>
   )
 }
