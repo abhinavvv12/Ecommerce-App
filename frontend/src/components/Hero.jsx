21a270a2926f3d5bfc3ca6945c0ec3eeba188925
Abhinav <abhinav.r1296@gmail.com>
2026-07-13 22:57:25 +0530

    navbar updates

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='flex flex-col sm:flex-row border border-gray-400
+    ' >
+        {/* hero left side */}
+
+        <div></div>
+      
+    </div>
+  )
+}
+
+export default Hero
--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 const Collection = () => {
   return (
     <div>
-      <h1>Collection</h1>
     </div>
   )
 }
