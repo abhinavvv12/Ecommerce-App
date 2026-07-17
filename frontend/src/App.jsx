368fcd4af70a128999aa770051b3f512e6116ca9
Abhinav <abhinav.r1296@gmail.com>
2026-07-17 21:01:42 +0530

    Building Collection Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,13 @@ import Collection from './pages/Collection'
 // import PlaceOrder from './pages/PlaceOrder'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import SearchBar from './components/SearchBar'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:[9vw]'>
       <Navbar />
+      <SearchBar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/collection' element={<Collection />} />
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div>
       <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
         <div>
-          <img src={assets.logo} alt="" />
+          <img className='w-36' src={assets.logo} alt="" />
           <p className='w-full md:w-2/3 text-gray-600'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Mollitia dolorum maiores ipsam sequi illum ut voluptatum vel
