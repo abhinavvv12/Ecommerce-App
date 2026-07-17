3d59676bb810972f033856b9fd60224e205493cf
Abhinav <abhinav.r1296@gmail.com>
2026-07-17 18:47:16 +0530

    footer section added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Collection from './pages/Collection'
 // import PlaceOrder from './pages/PlaceOrder'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
         <Route path='/place-order' element = {<PlaceOrder/>} />
         <Route path='/orders' element = {<Orders/>} /> */}
       </Routes>
+      <Footer />
     </div>
   )
 }
