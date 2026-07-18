b2d4218b0f5fee12f03669946f588c2a34a4c73f
Abhinav <abhinav.r1296@gmail.com>
2026-07-18 11:53:54 +0530

    Product page Features added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,26 +2,35 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Collection from './pages/Collection'
-// import PlaceOrder from './pages/PlaceOrder'
+import PlaceOrder from './pages/PlaceOrder'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import Orders from './pages/Orders'
+import {ToastContainer, toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:[9vw]'>
+      <ToastContainer />
       <Navbar />
       <SearchBar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/collection' element={<Collection />} />
-        {/* <Route path='/about' element = {<About/>} />
+        <Route path='/about' element = {<About/>} />
         <Route path='/contact' element = {<Contact/>} />
         <Route path='/product/:productId' element = {<Product/>} />
         <Route path='/Cart' element = {<Cart/>} />
         <Route path='/Login' element = {<Login/>} />
         <Route path='/place-order' element = {<PlaceOrder/>} />
-        <Route path='/orders' element = {<Orders/>} /> */}
+        <Route path='/orders' element = {<Orders/>} />
       </Routes>
       <Footer />
     </div>
