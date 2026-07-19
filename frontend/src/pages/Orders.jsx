2d58f71f08840151e23b98136326cc8ce23e33fc
Abhinav <abhinav.r1296@gmail.com>
2026-07-19 09:36:25 +0530

    orders Section done

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Title from '../components/Title'
+import { ShopContext } from '../context/ShopContext'
 
 const Orders = () => {
+
+  const { products, currency } = useContext(ShopContext);
   return (
-    <div>
-      Orders
+    <div className='border-t pt-16'>
+      <div className='text-2xl'>
+        <Title text1={'MY'} text2={'ORDERS'} />
+      </div>
+
+      <div>
+        {
+          products.slice(1, 4).map((item, index) => (
+            <div key={index} className='py-4 border-t border-b text-gray-700 flex flex-col md:flex-row md:items-center md:justify-between gap-4'>
+              <div className='flex items-start gap-6 text-sm'>
+                <img className='w-16 sm:w-20' src={item.image[0]} alt="" />
+                <div>
+                  <p className='sm:text-base font-medium'>{item.name}</p>
+                  <div className='flex items-center gap-3 mt-2 text-base text-gray-600'>
+                    <p>{currency}{item.price}</p>
+                    <p>Quantity: 1</p>
+                    <p>Size: M</p>
+                  </div>
+                  <p className='mt-2'>Date: <span className='text-gray-400'>25,July,2026</span></p>
+                </div>
+              </div>
+              <div className="flex flex-col sm:flex-row items-start sm:items-center gap-4 md:min-w-70 justify-between">
+                <div className="flex items-center gap-2">
+                  <span className="w-2 h-2 rounded-full bg-green-500"></span>
+                  <p className="text-sm md:text-base whitespace-nowrap">
+                    Ready to Ship
+                  </p>
+                </div>
+
+                <button className= " cursor-pointer border border-gray-300 px-5 py-2 text-sm rounded hover:bg-gray-100 transition">
+                  Track Order
+                </button>
+              </div>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
